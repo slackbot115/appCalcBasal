18567c5bf33a085e2c5d92dbc695ddbb7bab6877
slackbot115 <viniccius.carvalho22@gmail.com>
2019-03-27 08:00:58 +0900

    Project's Initial Commit

---
+++ www/components/acoes.js
@@ -0,0 +1,35 @@
+var radio = null;
+var taxa = null;
+var peso = $('#peso');
+var altura = $('#altura');
+var idade = $('#altura');
+
+$("input:radio").click(function(){
+  radio = $(this).val();
+});
+
+$('select').change(function(){
+  taxa = $('select option:selected').val();
+});
+
+var calculoHomem = function(){
+  return (taxa * (66 + (13.7 * parseFloat(peso.val()))) + (5 * parseFloat(altura.val())) - (6.8 * parseFloat(idade.val())));
+};
+
+var calculoMulher = function(){
+  return (taxa * (655 + (9.6 * parseFloat(peso.val()))) + (1.8 * parseFloat(altura.val())) - (4.7 * parseFloat(idade.val())));
+};
+
+$('button').click(function(){
+  if(radio == "homem"){
+    //$.cookie("resposta", limitarTam(calculoHomen()));
+    $('#res').val(limitarTam(calculoHomem()));
+  }else{
+    //$.cookie("resposta", limitarTam(calculoMulher()));
+    $('#res').val(limitarTam(calculoMulher()));
+  }
+});
+
+var limitarTam = function(str){
+  return str.toFixed(2);
+}
